0d4d772f744e3093a80a9f46d202652a692a6314
Kunal Behrunani <kunal.behrunani@gmail.com>
2021-08-02 18:49:19 +0530

    Create magicEightBall.js

---
+++ magicEightBall.js
@@ -0,0 +1,28 @@
+let userName = '';
+(userName==='')?(console.log('Hello!')):(console.log(`Hello, ${userName}!`));
+let userQuestion='Will I become a proficient Javascript Developer by the End of this year?';
+console.log(userQuestion);
+let randomNumber;
+randomNumber = Math.floor(Math.random()*9);
+let eightBall = '';
+switch(randomNumber){
+  case 0:eightBall='It is certain';
+        break;
+  case 1:eightBall='It is certain';
+        break;
+  case 2:eightBall='It is decidedly so';
+        break;
+  case 3:eightBall='Reply hazy try again';
+        break; 
+  case 4:eightBall='Cannot predict now';
+        break;
+  case 5:eightBall='Do not count on it';
+        break;
+  case 6:eightBall='My sources say no';
+        break;
+  case 7:eightBall='Outlook not so good';
+        break;
+  case 8:eightBall='Signs point to yes';
+        break;
+};
+console.log(eightBall);
